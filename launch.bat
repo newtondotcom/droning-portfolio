ec4354e22d5bd7bb1eb2ed9414c2a615df35b477
Robin Augereau <102476446+newtondotcom@users.noreply.github.com>
2023-08-03 16:18:28 +0200

    🗑️ del

--- launch.bat
+++
@@ -1,7 +0,0 @@
-set path=%path%:C:\Users\m23l604\Downloads\bin
-set path=%path%;C:\Users\m23l604\Downloads\PortableGit\bin
-set path=%path%;C:\Users\m23l604\Downloads\PortableGit\mingw64\bin
-set path=%path%;C:\Users\m23l604\Downloads\node-v18.17.0-win-x64
-C:
-cd C:\Users\m23l604\Downloads\droning-portfolio
-npm run dev
