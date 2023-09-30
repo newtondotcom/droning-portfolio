591cf36a401d008b82d49e85bfc7d16c9f46b81c
ROBIÑHO <asphalt9fr@gmail.com>
2023-09-30 13:14:05 +0200

    🔗https link for medias

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,15 +2,15 @@ const db =
 [
     {
       "name": "La-Tranche-Sur-Mer, France",
-      "videoLink": "http://144.91.123.186:999/latranche/output.m3u8",
-      "thumbnail": "http://144.91.123.186:999/latranche/minia2low.png",
+      "videoLink": "https://medias.newton-creations.fr/latranche/output.m3u8",
+      "thumbnail": "https://medias.newton-creations.fr/latranche/minia2low.png",
       "description": "Une visite virtuelle d'une maison en bord de mer, pleine de vie. Ce genre de vidéos, pleine de vie, peuvent êtres réalisées sur des propriétés privées et ce pour tout type d'activité.",
       "key": "0"
     },
     {
       "name": "Saint-Germain-en-Laye, France",
-      "videoLink": "http://144.91.123.186:999/mhouse/whouse.m3u8",
-      "thumbnail": "http://144.91.123.186:999/mhouse/minialow.png",
+      "videoLink": "https://medias.newton-creations.fr/mhouse/whouse.m3u8",
+      "thumbnail": "https://medias.newton-creations.fr/mhouse/minialow.png",
       "description": "Une maison de plus de 200 m² en plein milieu de la forêt. La visite immbolière par drone permet de mieux se rendre compte des volumes contrairement classique.",
       "key": "1"
     }
