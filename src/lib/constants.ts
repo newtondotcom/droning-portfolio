8af760b602a1c4483e2a179c9536ee8a4fbc8f4d
newtondotcom <asphalt9fr@gmail.com>
2023-11-20 10:32:38 +0100

    new webhook cause of spam

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,8 @@
 const Constants = {
 delay : 200,
 delayAnimation : 500,
-discordWebhook : "https://discord.com/api/webhooks/1138932963464192070/naN-uxPR1AEU9fBX_tJTv7RIJOzBl9YLpsfohH-otcceLHlSSI7ttxi2I7ndXgGZuUg-",
+discordWebhook : "https://discord.com/api/webhooks/1176092436905328671/SLdvBCTTRZjETl9YxjLLXC3nQQVaQ60yrRgkG4f4rLag2ojGAvTMzXxHImgIZHEqZ0QH",
+// please dont spam this webhook we are civilized people
 color : 1752220,
 }
 
